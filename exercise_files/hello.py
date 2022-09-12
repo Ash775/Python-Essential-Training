01ef8f59bee1f1db17344e9382aa374b3b675eb8
Ashilol <ashenperera258@gmail.com>
2022-09-12 16:59:36 +0530

    data structure first commit

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -8,3 +8,18 @@ print(colored("Hello, World!", "yellow"))
 print(colored("Hello, World!", "blue"))
 print(colored("Hello, World!", "magenta"))
 print(colored("Hello, World!", "cyan"))
+
+my_Dictionary = {
+    'Apple': colored('A red frutit', 'red'),
+    'Bear' : 'A scary animal',
+
+}
+
+print(my_Dictionary['Apple'])
+
+my_List = [
+    [1],[2, 'boy'], [3, 34, 'lol']
+]
+
+print(my_List)
+print(len(my_List))
