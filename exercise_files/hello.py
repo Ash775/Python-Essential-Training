305e99cd4e6406bfcd00efbed66565a3f06fa2ad
Ashilol <ashenperera258@gmail.com>
2022-09-12 16:21:30 +0530

    termcolor challenge

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,10 @@
+import sys
 
-# Print "hello, world!" to the terminal
-print('Hello, World!')
+from termcolor import colored
+
+print(colored("Hello, World!", "red"))
+print(colored("Hello, World!", "green"))
+print(colored("Hello, World!", "yellow"))
+print(colored("Hello, World!", "blue"))
+print(colored("Hello, World!", "magenta"))
+print(colored("Hello, World!", "cyan"))
